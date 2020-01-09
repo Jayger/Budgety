8bd58dd89598fe25484a609b4ac63bcbb03a7aad
Jordan R <jaygerzero@gmail.com>
2020-01-08 21:49:38 -0500

    Display Month and Year'

--- app.js
+++ app.js
@@ -167,7 +167,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: 'budget__title--month'
     }
 
     var formatNumber = function(num, type){
@@ -290,6 +291,18 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function() {
+            var now, year, month, months;
+
+            now = new Date();
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.datelabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -396,6 +409,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
